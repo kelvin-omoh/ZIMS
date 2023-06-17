2c222eec10df5d24afc89a47eaab7f2bfb28c896
unknown <eessien396@gmail.com>
2023-06-17 23:10:50 +0100

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
+import  firebase  from "firebase/app";
+import "firebase/database";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,4 +21,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+
+
 export default app;
